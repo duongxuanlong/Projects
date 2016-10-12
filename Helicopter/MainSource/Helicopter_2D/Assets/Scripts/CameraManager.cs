110a96110ec0d9ace4bcb091a67d03051527306b
mtbmt <mtbmt.0252@gmail.com>
2016-10-12 08:58:33 +0700

    Add files via upload

---
+++ Helicopter/MainSource/Helicopter_2D/Assets/Scripts/CameraManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraManager : MonoBehaviour {
+
+
+	public float m_cameraMoveSpeed;
+	private float cameraMoveSpeed;
+	public GameManager gameManager { get; set; }
+
+
+	// Use this for initialization
+	public void CameraInit () {
+		cameraMoveSpeed = m_cameraMoveSpeed / gameManager.pixelPerUnit;
+	}
+	
+	// Update is called once per frame
+	public void CameraUpdate() {
+		transform.position = transform.position + Vector3.right * cameraMoveSpeed * Time.deltaTime;
+
+	}
+}
